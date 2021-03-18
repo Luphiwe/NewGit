90b8ba1e3bc0ca1b5ae5ab3e477bd806e46322d8
sluphiwe@gmail.com <sluphiwe@gmail.com>
2021-03-18 19:56:14 +0200

    Modified integer sum and a double sum in the program.

--- src/za/ac/cput/git/Git.java
+++ src/za/ac/cput/git/Git.java
@@ -10,9 +10,24 @@ package za.ac.cput.git;
  * @author CPUT
  */
 public class Git {
-    
+    /**
+     * this adds two integers
+     * @param a
+     * @param b
+     * @return 
+     */
      public static int intergerSum(int a, int b) {
         
         return a+b;
     }
+     /** 
+      * this adds two doubles
+      * @param c
+      * @param d
+      * @return 
+      */
+     
+     public static double doubleSum(int c, int d) {
+         return c+d;
+     }
 }
